a27b10a5399db84bb4bc002b5f0132648e7e52aa
ShuEugene <shueugene@hotmail.com>
2022-08-31 21:29:45 +0300

    HW3.1 is corrected'n'completed.

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,44 @@
 //  Домашнее задание 3.1 (Урока №5. Условный оператор. Часть 1)
 public class Main {
     public static void main(String[] args) {
-        byte age = 5;
-        hw3_1_1(age);
-        hw3_1_2(age);
-        hw3_1_3(121);
-    }
-
 //  ДЗ 3.1.1
-    static void hw3_1_1(int age) {
-        if (age >= 18) System.out.println("\nПоздравляю с совершеннолетием!");
-        else System.out.println("\nСовершеннолетие ещё наступило. Подожди немного, пожалуйста.");
-    }
+        byte age = 5;
+        System.out.println(commingOfAge(age));
 
 //  ДЗ 3.1.2
-    static void hw3_1_2(int age) {
-        if (age >= 24)
-            System.out.println("\nПоздравляем с окончанием университета " +
-                    "и получением возможности трудоустроиться!");
-        else if (age >= 18)
-            System.out.println("\nПоздравляем с выпуском из школы! " +
-                "Теперь можно поступить в университет.");
-        else if (age >= 7)
-            System.out.println("\nШкола - первый шаг на пути к грамотности и самосовершенствованию." +
-                    " Пусть лёгок и светел будет твой путь!");
-        else System.out.println("\nПодростай малышь, не спеши. Всё у тебя ещё впереди.");
-    }
+        System.out.println(ageRelatedOpportunityes(age));
 
 //  ДЗ 3.1.3
-    public static byte wagonCapacity = 120;
-    public static byte sittingPlaces = 60;
-    static void hw3_1_3(int ticketNumber) {
-        System.out.println(theOpportunityToLeave(ticketNumber));
+        byte wagonCapacity = 120;
+        byte sittingPlaces = 60;
+        int ticketNumber = 121;
+        System.out.println(theOpportunityToGo(wagonCapacity, sittingPlaces, ticketNumber));
     }
-    public static String theOpportunityToLeave(int tn) {
-        if (tn > wagonCapacity) return "\nСвободных мест в этом вагоне уже не осталось." +
+
+    static String commingOfAge(int age) {
+        if (age >= 18) return "\nПоздравляю с совершеннолетием!";
+        else return "\nСовершеннолетие ещё наступило. Подожди немного, пожалуйста.";
+    }
+
+    static String ageRelatedOpportunityes(int age) {
+        if (age >= 24)
+            return "\nПоздравляем с окончанием университета" +
+                    " и получением возможности трудоустроиться!";
+        else if (age >= 18)
+            return "\nПоздравляем с выпуском из школы! " +
+                    "Теперь можно поступить в университет.";
+        else if (age >= 7)
+            return "\nШкола - первый шаг на пути к грамотности и самосовершенствованию." +
+                    " Пусть лёгок и светел будет твой путь!";
+        else return "\nПодростай малышь, не спеши. Всё у тебя ещё впереди.";
+    }
+
+    static String theOpportunityToGo(int wagonCapacity, int sittingPlaces, int ticketNumber) {
+        if (ticketNumber > wagonCapacity) return "\nСвободных мест в этом вагоне нет." +
                 " Пройдите, пожалуйста, в следующий.";
-        else if (tn > sittingPlaces) return "\nВсе места для сидения в вагоне заняты." +
+        else if (ticketNumber > sittingPlaces) return "\nВсе места для сидения в вагоне заняты." +
                 " Вы можете поехать стоя.";
-        else if (tn > 0) return "\nВ вагоне ещё есть места для сидения. Приятного пути!";
+        else if (ticketNumber > 0) return "\nВ вагоне ещё есть места для сидения. Приятного пути!";
         else return "Проезд без билета запрещён. Приобретите, пожалуйста, билет.";
     }
 }
