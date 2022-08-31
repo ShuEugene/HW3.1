a438301598d0c5b6b071595060b1bbfb1957427b
ShuEugene <shueugene@hotmail.com>
2022-08-31 23:59:56 +0300

    HW3.1 is corrected, completed'n'supplement.

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,32 @@
 //  Домашнее задание 3.1 (Урока №5. Условный оператор. Часть 1)
 public class Main {
     public static void main(String[] args) {
-//  ДЗ 3.1.1
-        byte age = 5;
+//  ДЗ 3.1.1(+3)
+        byte age = 1;
         System.out.println(commingOfAge(age));
 
-//  ДЗ 3.1.2
+//  ДЗ 3.1.2(+3)
         System.out.println(ageRelatedOpportunityes(age));
 
-//  ДЗ 3.1.3
+//  ДЗ 3.1.3(+3)
         byte wagonCapacity = 120;
         byte sittingPlaces = 60;
         int ticketNumber = 121;
         System.out.println(theOpportunityToGo(wagonCapacity, sittingPlaces, ticketNumber));
+
+//  ДЗ 3.1.7.1
+        System.out.println("\nЕсли возраст человека равен " + age +
+                ", то ему сто́ит ходить " + placeOfEmployement(age) + ".");
+
+//  ДЗ 3.1.7.2
+        System.out.println(canRide(age));
+
+//  ДЗ 3.1.7.3 вар.1
+        int one = 2;
+        int two = 1;
+        int free = 3;
+        System.out.printf("\nИз чисел %d, %d и %d бо́льшим является %s.",
+                one, two, free, aLargerNumber(one, two, free));
     }
 
     static String commingOfAge(int age) {
@@ -40,5 +54,29 @@ public class Main {
                 " Вы можете поехать стоя.";
         else if (ticketNumber > 0) return "\nВ вагоне ещё есть места для сидения. Приятного пути!";
         else return "Проезд без билета запрещён. Приобретите, пожалуйста, билет.";
+    }
+
+    static String placeOfEmployement(int age) {
+        if (age > 24) return "на работу";
+            else if (age >= 18 && age <= 24) return "в университет";
+                else if (age >= 7 && age < 18) return "в школу";
+                else if (age >= 2 && age < 7) return "в детский сад";
+        return "рядом с мамой";
+    }
+
+    static String canRide(int age) {
+        if (age > 14) return "\nДобро пожаловать на аттракцион!";
+        else if (age >= 5 && age <= 14) return "\nТебе можно кататься только со старшими.";
+        return "\nИзвини, малышь, но тебе пока сюда нельзя.";
+    }
+
+    static String aLargerNumber(int one, int two, int free) {
+        if (one == two && one == free) return "никакое; все три числа равны";
+        else if (one == two && one > free) return "первое и второе; они равны";
+        else if (two == free && two >one) return "второе и третье; они равны";
+        else if (free == one && free > two) return "первое и третье; они равны";
+        else if (one > two && one > free) return "первое";
+        else if (two > one && two > free) return "второе";
+        return "третье";
     }
 }
