1949dc05dc007156ca1239ce183a28c3a5ec5c54
ShuEugene <shueugene@hotmail.com>
2022-08-31 19:02:56 +0300

    HW3.1.1 is completed.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
 public class Main {
     public static void main(String[] args) {
 //  Домашнее задание 3.1 (Урока №5. Условный оператор. Часть 1)
-
+        byte age = 7;
+        hw3_1_1(age);
     }
+
+    //        ДЗ 3.1.1
+    public static void hw3_1_1(int age) {
+        if (age >= 18) System.out.println("\nПоздравляю с совершеннолетием!");
+        else System.out.println("\nСовершеннолетие ещё наступило. Подожди немного, пожалуйста.");
+    }
+
 }
